795c61c39c83af3fc952ec4e6ce3de3c98732221
Jeffrey S. Morgan <jeff.morgan@leandog.com>
2018-05-10 18:57:36 -0500

    Added Cucumber Support

---
+++ src/test/java/MyStepdefs.java
@@ -0,0 +1,55 @@
+import cucumber.api.PendingException;
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class MyStepdefs {
+    @Given("^I am on the login page$")
+    public void iAmOnTheLoginPage() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^I enter a valid username and password$")
+    public void iEnterAValidUsernameAndPassword() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^I should see the welcome page$")
+    public void iShouldSeeTheWelcomePage() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^I enter an invalid username or password$")
+    public void iEnterAnInvalidUsernameOrPassword() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^I expect to be on the login page$")
+    public void iExpectToBeOnTheLoginPage() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @And("^I expect to see an error message$")
+    public void iExpectToSeeAnErrorMessage() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^I am missing a username$")
+    public void iAmMissingAUsername() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @And("^I expect to see an missing field error$")
+    public void iExpectToSeeAnMissingFieldError() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+}
---
+++ src/test/java/com/agile/and/beyond/RunCukesTest.java
@@ -0,0 +1,8 @@
+package com.agile.and.beyond;
+
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+public class RunCukesTest {
+}
