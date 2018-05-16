47a40a17d7bf3191c309676fd791ff8b200d4410
Jeffrey S. Morgan <jeff.morgan@leandog.com>
2018-05-16 14:00:00 -0400

    added home page object

---
+++ src/test/java/com/agile/and/beyond/pages/HomePage.java
@@ -0,0 +1,11 @@
+package com.agile.and.beyond.pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class HomePage extends BasePageObject {
+
+    public HomePage(WebDriver driver) {
+        super(driver);
+        url = "http://localhost:8080";
+    }
+}
