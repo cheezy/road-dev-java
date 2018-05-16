b2c3a723c1bca3a9a0a45df74182184a8e2ffea7
Jeffrey S. Morgan <jeff.morgan@leandog.com>
2018-05-16 13:30:48 -0400

    Changed Cukes to integration-test target

--- src/test/java/com/agile/and/beyond/RunCukesIT.java
+++ src/test/java/com/agile/and/beyond/RunCukesIT.java
@@ -1,8 +1,9 @@
 package com.agile.and.beyond;
 
 import cucumber.api.junit.Cucumber;
+import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-public class RunCukesTest {
+public class RunCukesIT {
 }
