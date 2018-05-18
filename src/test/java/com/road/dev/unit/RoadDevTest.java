a8ebc03dc296c3281a422973a590d3ea16356760
Jeffrey S. Morgan <jeff.morgan@leandog.com>
2018-05-18 13:19:51 -0400

    added unit test

---
+++ src/test/java/com/road/dev/unit/RoadDevTest.java
@@ -0,0 +1,12 @@
+package com.road.dev.unit;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class RoadDevTest {
+
+    @Test
+    public void hookup() {
+        assertTrue(true);
+    }
+}
