4b05c7631b19e8320273fbdc06d6804a1029b081
Jeffrey S. Morgan <jeff.morgan@leandog.com>
2018-05-18 13:08:12 -0400

    Automatically starting tomcat prior to cucumber run.

--- src/test/java/com/agile/and/beyond/RunCukesIT.java
+++ src/test/java/com/agile/and/beyond/RunCukesIT.java
@@ -1,7 +1,6 @@
 package com.agile.and.beyond;
 
 import cucumber.api.junit.Cucumber;
-import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
